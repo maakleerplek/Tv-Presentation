6fec760ffdf00af020f7eee62e9e1d81335edbed
eggmansmile <ruben.ryckaert89@gmail.com>
2026-03-26 23:34:46 +0100

    chore: remove obsolete CJS debug scripts (replaced by ESM .js versions)

--- data-fetcher/check-images.cjs
+++
@@ -1,7 +0,0 @@
-// NOTE: Run this with: node --input-type=module < check-images.cjs
-// Or better: use check-images.js instead (ESM version).
-// This file is kept for compatibility but redirects to the .js version.
-//
-// Since this package is "type": "module", .cjs files use CommonJS but cannot
-// import from scraper-config.js (ESM). Use check-images.js instead.
-console.warn('Use check-images.js instead (run: node check-images.js)');
