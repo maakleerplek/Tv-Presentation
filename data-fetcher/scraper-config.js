d94dd67d98f186828d59efeafba736a9fd391e94
eggmansmile <ruben.ryckaert89@gmail.com>
2026-03-26 19:25:09 +0100

    Update scraper-config to translated maakleerplek URL

--- data-fetcher/scraper-config.js
+++ data-fetcher/scraper-config.js
@@ -1,8 +1,5 @@
 // Central config for data-fetcher scraper targets.
 // Set MAAKLEERPLEK_URL here instead of environment variables if preferred.
 
-export const MAAKLEERPLEK_URL = 'https://maakleerplek.be';
+export const MAAKLEERPLEK_URL = 'https://maakleerplek-be.translate.goog/?_x_tr_sl=en&_x_tr_tl=nl&_x_tr_hl=nl&_x_tr_pto=wapp';
 
-// Example translation proxy URL supported:
-// export const MAAKLEERPLEK_URL =
-//   'https://maakleerplek-be.translate.goog/?_x_tr_sl=en&_x_tr_tl=nl&_x_tr_hl=nl&_x_tr_pto=wapp';
