69bd052243edbd760d2dbbe5580235d99b8b3ded
eggmansmile <ruben.ryckaert89@gmail.com>
2026-03-26 18:39:44 +0100

    changed to english

--- Reworked website/app/layout.tsx
+++ Reworked website/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="nl" className={`${spaceGrotesk.variable} ${jetbrainsMono.variable}`}>
+    <html lang="en" className={`${spaceGrotesk.variable} ${jetbrainsMono.variable}`}>
       <body className="font-sans antialiased" suppressHydrationWarning>{children}</body>
     </html>
   );
